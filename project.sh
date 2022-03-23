a2e0d5dfc3e9845a47cbe99c09fee8ea8916185a
Max <msf9542@rit.edu>
2022-03-22 20:30:46 -0400

    minor change

--- project.sh
+++ project.sh
@@ -25,18 +25,18 @@ function disk () {
 
 #process metrics
 function process () {
-	cpu_apm1=$(ps -aux | grep APM1 | head -n -1 | awk '{print $3}' | tr -d '\n' )
-	cpu_apm2=$(ps -aux | grep APM2 | head -n -1 | awk '{print $3}' | tr -d '\n' )
-	cpu_apm3=$(ps -aux | grep APM3 | head -n -1 | awk '{print $3}' | tr -d '\n' )
-	cpu_apm4=$(ps -aux | grep APM4 | head -n -1 | awk '{print $3}' | tr -d '\n' )
-	cpu_apm5=$(ps -aux | grep APM5 | head -n -1 | awk '{print $3}' | tr -d '\n' )
-	cpu_apm6=$(ps -aux | grep APM6 | head -n -1 | awk '{print $3}' | tr -d '\n' )
-	mem_apm1=$(ps -aux | grep APM1 | head -n -1 | awk '{print $4}' | tr -d '\n' )
-	mem_apm2=$(ps -aux | grep APM2 | head -n -1 | awk '{print $4}' | tr -d '\n' )
-	mem_apm3=$(ps -aux | grep APM3 | head -n -1 | awk '{print $4}' | tr -d '\n' )
-	mem_apm4=$(ps -aux | grep APM4 | head -n -1 | awk '{print $4}' | tr -d '\n' )
-	mem_apm5=$(ps -aux | grep APM5 | head -n -1 | awk '{print $4}' | tr -d '\n' )
-	mem_apm6=$(ps -aux | grep APM6 | head -n -1 | awk '{print $4}' | tr -d '\n' )
+	cpu_apm1=$(ps -au | grep APM1 | head -n -1 | awk '{print $3}' | tr -d '\n' )
+	cpu_apm2=$(ps -au | grep APM2 | head -n -1 | awk '{print $3}' | tr -d '\n' )
+	cpu_apm3=$(ps -au | grep APM3 | head -n -1 | awk '{print $3}' | tr -d '\n' )
+	cpu_apm4=$(ps -au | grep APM4 | head -n -1 | awk '{print $3}' | tr -d '\n' )
+	cpu_apm5=$(ps -au | grep APM5 | head -n -1 | awk '{print $3}' | tr -d '\n' )
+	cpu_apm6=$(ps -au | grep APM6 | head -n -1 | awk '{print $3}' | tr -d '\n' )
+	mem_apm1=$(ps -au | grep APM1 | head -n -1 | awk '{print $4}' | tr -d '\n' )
+	mem_apm2=$(ps -au | grep APM2 | head -n -1 | awk '{print $4}' | tr -d '\n' )
+	mem_apm3=$(ps -au | grep APM3 | head -n -1 | awk '{print $4}' | tr -d '\n' )
+	mem_apm4=$(ps -au | grep APM4 | head -n -1 | awk '{print $4}' | tr -d '\n' )
+	mem_apm5=$(ps -au | grep APM5 | head -n -1 | awk '{print $4}' | tr -d '\n' )
+	mem_apm6=$(ps -au | grep APM6 | head -n -1 | awk '{print $4}' | tr -d '\n' )
 
 	echo "${runtime},${cpu_apm1},${cpu_apm2},${cpu_apm3},${cpu_apm4},${cpu_apm5},${cpu_apm6},${mem_apm1},${cpu_apm2},${cpu_apm3},${cpu_apm4},${cpu_apm5},${cpu_apm6}" >> process_metrics.csv
 }
